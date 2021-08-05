6ab71997203f3430253feb9e0c8596cf4df6e760
AfifArdhiansyah <afifardhiansyah@gmail.com>
2021-08-05 15:21:54 +0700

    commit kedua

--- app/src/main/java/com/example/sinau/MainActivity.kt
+++ app/src/main/java/com/example/sinau/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.sinau
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.LayoutInflater
+import com.example.sinau.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var message: String
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.activityMainButtonLogin.setOnClickListener{
+            println("Button dipentet")
+            val intentToSecondActivity = Intent(this, SecondActivity::class.java)
+            startActivity(intentToSecondActivity)
+        }
     }
 }
